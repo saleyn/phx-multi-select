0dd9321ab13e6710928685f5220a76f576be1d8b
Serge Aleynikov <saleyn@gmail.com>
2023-01-28 12:09:09 -0500

    Update project

--- mix.exs
+++ mix.exs
@@ -6,8 +6,7 @@ defmodule MultiSelect.MixProject do
       app: :multi_select,
       version: "0.1.0",
       elixir: "~> 1.14",
-      elixirc_paths: elixirc_paths(Mix.env()),
-      #start_permanent: Mix.env() == :prod,
+      elixirc_paths: ["lib"],
       deps: deps()
     ]
   end
@@ -17,14 +16,9 @@ defmodule MultiSelect.MixProject do
   # Type `mix help compile.app` for more information.
   def application do
     [
-      mod: {MultiSelectExample.Application, []},
-      extra_applications: [:logger, :runtime_tools]
+      extra_applications: []
     ]
   end
-
-  # Specifies which paths to compile per environment.
-  defp elixirc_paths(:test), do: ["lib", "test/support"]
-  defp elixirc_paths(_), do: ["lib"]
 
   # Specifies your project dependencies.
   defp deps do
@@ -32,9 +26,6 @@ defmodule MultiSelect.MixProject do
       {:phoenix,             "~> 1.7.0-rc.2", override: true},
       {:phoenix_html,        "~> 3.0"},
       {:phoenix_live_view,   "~> 0.18.3"},
-      {:esbuild,             "~> 0.5",    runtime: Mix.env() == :dev},
-      {:tailwind,            "~> 0.1.8",  runtime: Mix.env() == :dev},
-      {:gettext,             "~> 0.20"},
     ]
   end
 end
